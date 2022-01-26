df34fbae1fdb73d5d0b4c9973d0923c6a2639a91
kamal14rajput <kamal14rajput@gmail.com>
2022-01-26 19:55:30 +0530

    UPDATED: retrieve employee in data range

--- src/main/java/com/bridgelabz/EmployeePayrollException.java
+++ src/main/java/com/bridgelabz/EmployeePayrollException.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class EmployeePayrollException extends Exception {
 	enum ExceptionType {
-		CONNECTION_FAIL, CANNOT_RETRIEVE_DATA, CANNOT_EXECUTE_QUERY, UPDATE_FAILED
+		CONNECTION_FAIL, CANNOT_EXECUTE_QUERY, UPDATE_FAILED
 	}
 
 	ExceptionType exceptionType;
