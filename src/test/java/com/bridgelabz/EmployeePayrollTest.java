a2f06625285671a7b80aaab541601411eb626e46
kamal14rajput <kamal14rajput@gmail.com>
2022-01-26 19:34:49 +0530

    UPDATED: test for salary updated

--- src/test/java/com/bridgelabz/EmployeePayrollTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollTest.java
@@ -6,14 +6,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.bridgelabz.EmployeePayrollData;
+import com.bridgelabz.EmployeePayrollException;
 import com.bridgelabz.EmployeePayrollService;
 import static com.bridgelabz.EmployeePayrollService.IOService.DATABASE_IO;
 
 public class EmployeePayrollTest {
 	@Test
-	public void givenEmployeePayrollData_WhenRetrieved_ShouldMatchNumberOfEmployees() {
+	public void givenEmployeePayrollData_WhenRetrieved_ShouldMatchNumberOfEmployees() throws EmployeePayrollException {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DATABASE_IO);
 		Assert.assertEquals(3, employeePayrollData.size());
+	}
+
+	@Test
+	public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDatabase() throws EmployeePayrollException {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DATABASE_IO);
+		employeePayrollService.updateEmployeeSalary("Charlie", 30000);
+		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDatabase("Charlie");
+		Assert.assertTrue(result);
 	}
 }
