f03f71671aa698b119614871b983d692d37ac4a7
kamal14rajput <kamal14rajput@gmail.com>
2022-01-26 19:34:16 +0530

    UPDATED: salary updated

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -9,9 +9,40 @@ public class EmployeePayrollService {
 
 	private List<EmployeePayrollData> employeePayrollData;
 
-	public List<EmployeePayrollData> readEmployeePayrollData(IOService ioService) {
-		if (ioService.equals(IOService.DATABASE_IO))
-			return this.employeePayrollData = new EmployeePayrollDatabaseService().readData(null, null);
-		return this.employeePayrollData;
+	public void updateEmployeeSalary(String name, double salary) throws EmployeePayrollException {
+		int result = new EmployeePayrollDatabaseService().updateEmployeeData(name, salary);
+		if (result == 0)
+			throw new EmployeePayrollException("Salary update failed",
+					EmployeePayrollException.ExceptionType.UPDATE_FAILED);
+		EmployeePayrollData employeePayrollData = this.getEmployeePayrollData(name);
+		if (employeePayrollData != null)
+			employeePayrollData.salary = salary;
+	}
+
+	private EmployeePayrollData getEmployeePayrollData(String name) {
+		return this.employeePayrollData.stream()
+				.filter(employeePayrollDataItem -> employeePayrollDataItem.name.equals(name)).findFirst().orElse(null);
+	}
+
+	public boolean checkEmployeePayrollInSyncWithDatabase(String name) throws EmployeePayrollException {
+		try {
+			List<EmployeePayrollData> employeePayrollData = new EmployeePayrollDatabaseService()
+					.getEmployeePayrollData(name);
+			return employeePayrollData.get(0).equals(getEmployeePayrollData(name));
+		} catch (EmployeePayrollException employeePayrollException) {
+			throw new EmployeePayrollException("Cannot execute query",
+					EmployeePayrollException.ExceptionType.CANNOT_EXECUTE_QUERY);
+		}
+	}
+
+	public List<EmployeePayrollData> readEmployeePayrollData(IOService ioService) throws EmployeePayrollException {
+		try {
+			if (ioService.equals(IOService.DATABASE_IO))
+				return this.employeePayrollData = new EmployeePayrollDatabaseService().readData();
+			return this.employeePayrollData;
+		} catch (EmployeePayrollException employeePayrollException) {
+			throw new EmployeePayrollException("Cannot execute query",
+					EmployeePayrollException.ExceptionType.CANNOT_EXECUTE_QUERY);
+		}
 	}
 }
