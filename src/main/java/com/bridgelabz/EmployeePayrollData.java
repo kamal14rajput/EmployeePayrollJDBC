49298332758935bda56b90061d02371f90898ede
kamal14rajput <kamal14rajput@gmail.com>
2022-01-26 19:33:56 +0530

    UPDATED: salary updated

--- src/main/java/com/bridgelabz/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -8,10 +8,30 @@ public class EmployeePayrollData {
 	public double salary;
 	public LocalDate startDate;
 
-	public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+	public EmployeePayrollData(Integer id, String name, Double salary) {
 		this.id = id;
 		this.name = name;
 		this.salary = salary;
+	}
+
+	public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+		this(id, name, salary);
 		this.startDate = startDate;
+	}
+
+	@Override
+	public String toString() {
+		return "ID = " + id + ", Name = " + name + ", Salary = " + salary;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object)
+			return true;
+		if (object == null || getClass() != object.getClass())
+			return false;
+		EmployeePayrollData employeePayrollData = (EmployeePayrollData) object;
+		return id == employeePayrollData.id && Double.compare(employeePayrollData.salary, salary) == 0
+				&& name.equals(employeePayrollData.name);
 	}
 }
