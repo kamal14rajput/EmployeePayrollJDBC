a215769fe08d5865ac85ce96b2958074ee83aa42
kamal14rajput <kamal14rajput@gmail.com>
2022-01-27 09:51:01 +0530

    UPDATED: number of male and female employee

--- src/main/java/com/bridgelabz/EmployeePayrollMain.java
+++ src/main/java/com/bridgelabz/EmployeePayrollMain.java
@@ -10,7 +10,7 @@ public class EmployeePayrollMain {
 	public static void main(String[] args) {
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 		String username = "root";
-		String password = "cadet4kamal";
+		String password = "******";
 		Connection connection;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
