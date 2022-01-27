ea3cd56c8b3f64fa89edcf99a6b626000b6be93b
kamal14rajput <kamal14rajput@gmail.com>
2022-01-27 09:51:08 +0530

    UPDATED: number of male and female employee

--- src/main/java/com/bridgelabz/EmployeePayrollDatabaseService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollDatabaseService.java
@@ -19,7 +19,7 @@ public class EmployeePayrollDatabaseService {
 	private Connection getConnection() throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 		String username = "root";
-		String password = "cadet4kamal";
+		String password = "******";
 		Connection connection = null;
 		System.out.println("Connecting to database: " + jdbcURL);
 		connection = DriverManager.getConnection(jdbcURL, username, password);
